bc9ecd25f181e7c8914432d14ab3edac7b27f854
“HariharasudhanAS” <harimailbox99@gmail.com>
2018-11-06 03:05:29 +0530

    Chapter 5

--- Lab/Chapter 5/Chap 5.r
+++ Lab/Chapter 5/Chap 5.r
@@ -34,6 +34,41 @@ library(boot)
 glm.fit = glm(mpg~horsepower, data=Auto)
 cv.err = cv.glm(Auto, glm.fit)
 cv.err$delta
+library(ISLR)
+set.seed(1)
+
+#random selection
+train = sample(392,196)
+
+# model fitting
+lm.fit = lm(mpg~horsepower, data=Auto, subset=train)
+summary(lm.fit)
+attach(Auto)
+
+# MSE in test set
+mean((mpg-predict(lm.fit, Auto))[-train]^2)
+
+# testing for quadratic and cubic
+lm.fit2 = lm(mpg~poly(horsepower,2),data=Auto,subset=train)
+mean((mpg-predict(lm.fit2, Auto))[-train]^2)
+lm.fit3 = lm(mpg~poly(horsepower,3),data=Auto,subset=train)
+mean((mpg-predict(lm.fit3, Auto))[-train]^2)
+
+# Different sample
+set.seed(2)
+train = sample(392, 196)
+lm.fit=lm(mpg~horsepower, data=Auto, subset=train)
+mean((mpg-predict(lm.fit, Auto))[-train]^2)
+lm.fit2 = lm(mpg~poly(horsepower,2),data=Auto,subset=train)
+mean((mpg-predict(lm.fit2, Auto))[-train]^2)
+lm.fit3 = lm(mpg~poly(horsepower,3),data=Auto,subset=train)
+mean((mpg-predict(lm.fit3, Auto))[-train]^2)
+
+# LOOCV
+library(boot)
+?cv.glm # default LOOCV
+glm.fit = glm(mpg~horsepower, data=Auto)
+cv.err = cv.glm(Auto, glm.fit)
 
 cv.error = rep(0,5)
 for ( i in 1:5){
--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Solutions to [ISLR](https://www-bcf.usc.edu/~gareth/ISL/)
 * [Chapter 2](https://github.com/HariharasudhanAS/ISLR-Exercises/tree/master/Chapter%202)
 * [Chapter 3](https://github.com/HariharasudhanAS/ISLR-Exercises/tree/master/Chapter%203)
 * [Chapter 4](https://github.com/HariharasudhanAS/ISLR-Exercises/tree/master/Chapter%204)
+* [Chapter 5](https://github.com/HariharasudhanAS/ISLR-Exercises/tree/master/Chapter%205)
 ### Labs
 * [Chapter 3](https://github.com/HariharasudhanAS/ISLR-Exercises/tree/master/Lab/Chapter%203)
 * [Chapter 4](https://github.com/HariharasudhanAS/ISLR-Exercises/tree/master/Lab/Chapter%204)
+* [Chapter 5](https://github.com/HariharasudhanAS/ISLR-Exercises/tree/master/Lab/Chapter%205)
 ### Other Resources
 * [References](https://github.com/HariharasudhanAS/ISLR-Exercises/blob/master/Papers.md)
